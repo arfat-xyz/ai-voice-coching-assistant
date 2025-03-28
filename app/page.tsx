d7e3d8339438dbce635efb107e93b96a0e0b52d9
arfat-xyz <arfatrahman08@gmail.com>
2025-03-28 16:12:44 +0600

    shadcn added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <>
-      <h1>Arfatu rRahman</h1>
-    </>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
